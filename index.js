cb1045881d731f1748a8eddea629767f6ebcdaeb
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-06-02 09:58:11 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names =[];
+function writeCards(names, event){
+    const newArray =[];
+    for(let i =0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newArray[i] = message;
+    }
+    return newArray;
+}
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+
+}
